afb87695942ea50c40dc7fa97ccd57195c987451
William Manley <william.manley@youview.com>
2012-06-20 15:05:06 +0100

    opencv templatematch: Remove no-op call to gst_templatematch_load_template
    We have just set filter->template to NULL, so
    gst_templatematch_load_template did nothing.

--- ext/opencv/gsttemplatematch.c
+++ ext/opencv/gsttemplatematch.c
@@ -187,7 +187,6 @@ gst_template_match_init (GstTemplateMatch * filter,
   filter->cvDistImage = NULL;
   filter->cvImage = NULL;
   filter->method = DEFAULT_METHOD;
-  gst_template_match_load_template (filter);
 }
 
 static void
