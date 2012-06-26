4aae9a2c6f59ecf4a3867fedac31039f8b72c9a7
David Rothlisberger <david@rothlis.net>
2012-06-26 09:17:11 +0100

    opencv templatematch: Remove superfluous comment & unused variable

--- tests/check/elements/opencv/templatematch-missing-template.sh
+++ tests/check/elements/opencv/templatematch-missing-template.sh
@@ -2,11 +2,6 @@
 
 # Test that templatematch produces an error if the requested template cannot be
 # loaded.
-#
-# This test must be run from the check/ directory as it will be under
-# `make check`.  We use $PWD below as uridecodebin requires an absolute path.
-
-image="$PWD/../files/blue-circle.png"
 
 GST_DEBUG=2 gst-launch \
     videotestsrc \
